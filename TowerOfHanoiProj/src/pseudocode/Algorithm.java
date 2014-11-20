1ccb821b3ccbcf4e10268cc205ec0b8ccba929ca
robotics <robotics@robotics-VB.(none)>
2014-11-20 22:19:48 +0900

    擬似コードのアップ

---
+++ TowerOfHanoiProj/src/pseudocode/Algorithm.java
@@ -0,0 +1,25 @@
+package pseudocode;
+
+import java.util.Iterator;
+
+//ブレインクラス
+public class Algorithm {
+
+	//コンストラクタ
+	Algorithm()
+	{
+		for (int i = 0; i < array.length; i++) {
+			円盤の生成
+			棒の生成
+			
+			
+			コンソールで入力待ち
+			
+			if Enterキー
+			{
+				次の処理を行う。（円盤の移動、）
+			}
+		}
+	}
+
+}
